cd7891742a2b5c88233f25c4764b6bdd75f785e7
Max <max@bischof.dk>
2020-11-20 21:19:29 +0100

    Hide app when window is losing focus

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import { useState } from 'react'
 import Exercise from './components/Exercise'
 import { useContent } from './hooks/contentful'
 
 function App() {
   const words = useContent('word')
-  return <div className="App">{words && <Exercise words={words} />}</div>
+  const [windowlostFocus, setwindowlostFocus] = useState()
+
+  document.addEventListener('blur', () => setwindowlostFocus(true))
+  document.addEventListener('visibilitychange', () => setwindowlostFocus(true))
+
+  return (
+    <div className="App">
+      {words && !windowlostFocus && <Exercise words={words} />}
+      {windowlostFocus && <div>Lost Focus</div>}
+    </div>
+  )
 }
 
 export default App

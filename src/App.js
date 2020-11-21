2df7beb95c49d7da2871d6fc23c7304a998a6fc1
Max <max@bischof.dk>
2020-11-21 11:44:46 +0100

    Add react router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import Exercise from './components/Exercise'
 import { useContent } from './hooks/contentful'
+import { Route } from 'react-router-dom'
 
 function App() {
   const words = useContent('word')
@@ -11,8 +12,13 @@ function App() {
 
   return (
     <div className="App">
-      {words && !windowlostFocus && <Exercise words={words} />}
-      {windowlostFocus && <div>Lost Focus</div>}
+      <Route exact path="/">
+        <h1>Form</h1>
+      </Route>
+      <Route path="/exercise">
+        {words && !windowlostFocus && <Exercise words={words} />}
+        {windowlostFocus && <div>Lost Focus</div>}
+      </Route>
     </div>
   )
 }

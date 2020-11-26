a27a09fa4d5db9fee9f50cb98bc5b280676a6ecf
Max <max@bischof.dk>
2020-11-26 14:25:15 +0100

    basic styling of results page

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router'
+import styled from 'styled-components'
 
 export default function Results({ db }) {
   const { testID } = useParams()
@@ -20,16 +21,31 @@ export default function Results({ db }) {
   }, [testID, db])
 
   return (
-    <>
-      <h2>Results from Test {testID}</h2>
-      {results &&
-        results.map((result) => {
-          return (
-            <p key={result.name}>
-              {result.name}: {result.result}
-            </p>
-          )
-        })}
-    </>
+    <StyledResults>
+      <h1>Alle Ergebnisse</h1>
+      <Ul>
+        {results &&
+          results.map((result) => {
+            return (
+              <Li key={result.name + result.result}>
+                {result.name}: {result.result}
+              </Li>
+            )
+          })}
+      </Ul>
+    </StyledResults>
   )
 }
+
+const StyledResults = styled.div`
+  text-align: center;
+`
+const Ul = styled.ul`
+  list-style-type: none;
+  padding: 0;
+  display: inline-block;
+`
+const Li = styled.li`
+  padding: 10px;
+  text-align: left;
+`

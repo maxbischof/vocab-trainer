f28e7e921f32c3298d88b4431237abfcf00ac195
Max <max@bischof.dk>
2020-11-27 10:17:53 +0100

    Add button test

--- src/components/TestLink.test.js
+++ src/components/TestLink.test.js
@@ -3,16 +3,14 @@ import TestLink from './TestLink'
 import { BrowserRouter as Router } from 'react-router-dom'
 import renderer from 'react-test-renderer'
 
-describe('my function or component', () => {
-  it('renders correctly', () => {
-    const testURL = '/tests/123456'
-    const tree = renderer
-      .create(
-        <Router>
-          <TestLink testURL={testURL} />
-        </Router>
-      )
-      .toJSON()
-    expect(tree).toMatchSnapshot()
-  })
+it('renders correctly', () => {
+  const testURL = '/tests/123456'
+  const tree = renderer
+    .create(
+      <Router>
+        <TestLink testURL={testURL} />
+      </Router>
+    )
+    .toJSON()
+  expect(tree).toMatchSnapshot()
 })

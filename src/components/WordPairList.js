1f6cf5f75ae84b0c3a14ddcccdaeb6ff21640bfe
Max <max@bischof.dk>
2020-11-28 14:54:43 +0100

    Refactor wordpairlist

--- src/components/WordPairList.js
+++ src/components/WordPairList.js
@@ -2,17 +2,17 @@ import React from 'react'
 import styled from 'styled-components'
 import WordPair from './WordPair'
 
-export default function WordpairList({ words, deleteWord }) {
+export default function WordPairList({ words, deleteWord }) {
   return (
-    <Ul>
+    <StyledWordPairList>
       {words.map((word, index) => (
         <WordPair word={word} index={index} deleteWord={deleteWord} />
       ))}
-    </Ul>
+    </StyledWordPairList>
   )
 }
 
-const Ul = styled.ul`
+const StyledWordPairList = styled.ul`
   list-style-type: none;
   padding: 0;
 `

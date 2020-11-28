b7390e5536b2917a7b308543a6aab7e4f3889a69
Max <max@bischof.dk>
2020-11-28 14:47:07 +0100

    Add wordpair component

---
+++ src/components/WordPair.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import styled from 'styled-components'
+import { ReactComponent as DeleteSVG } from '../icons/delete.svg'
+
+export default function WordPair({ word, deleteWord, index }) {
+  return (
+    <StyledWordpair key={word.foreign}>
+      <Words>
+        <b>{word.foreign}</b>
+        <small>{word.native}</small>
+      </Words>
+      <DeleteIcon onClick={() => deleteWord(index)} />
+    </StyledWordpair>
+  )
+}
+
+const StyledWordpair = styled.li`
+  padding: 10px;
+  border-bottom: 1px solid var(--text);
+  display: grid;
+  grid-template-columns: auto 40px;
+`
+
+const Words = styled.div`
+  grid-column: 1 / 2;
+  display: flex;
+  flex-direction: column;
+`
+
+const DeleteIcon = styled(DeleteSVG)`
+  grid-column: 2 / 3;
+  grid-row: 1 / 3;
+  display: grid;
+  align-items: center;
+  margin: 0 0 0 25px;
+`

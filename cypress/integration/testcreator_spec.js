cd6c8089c6e3d2606e09677febbd00f943bfc2ac
Max <max@bischof.dk>
2020-11-30 20:10:53 +0100

    Update cypress tests

--- cypress/integration/testcreator_spec.js
+++ cypress/integration/testcreator_spec.js
@@ -3,7 +3,7 @@ describe('TestCreator', () => {
     cy.visit('localhost:3000')
     cy.get('input[name="foreign"]').type('the candle')
     cy.get('input[name="native"]').type('die Kerze')
-    cy.contains('Wortpaar hinzufügen').click()
+    cy.contains('Add word').click()
 
     cy.contains('the candle')
     cy.contains('die Kerze')
@@ -13,11 +13,11 @@ describe('TestCreator', () => {
     cy.visit('localhost:3000')
     cy.get('input[name="foreign"]').type('the candle')
     cy.get('input[name="native"]').type('die Kerze')
-    cy.contains('Wortpaar hinzufügen').click()
+    cy.contains('Add word').click()
 
     cy.get('input[name="foreign"]').type('the tea')
     cy.get('input[name="native"]').type('der Tee')
-    cy.contains('Wortpaar hinzufügen').click()
+    cy.contains('Add word').click()
 
     cy.contains('the candle')
       .parent('div')
@@ -33,11 +33,11 @@ describe('TestCreator', () => {
     cy.visit('localhost:3000')
     cy.get('input[name="foreign"]').type('the candle')
     cy.get('input[name="native"]').type('die Kerze')
-    cy.contains('Wortpaar hinzufügen').click()
+    cy.contains('Add word').click()
 
     cy.get('input[name="foreign"]').type('the tea')
     cy.get('input[name="native"]').type('der Tee')
-    cy.contains('Wortpaar hinzufügen').click()
+    cy.contains('Add word').click()
 
     cy.get('button').contains('Test erstellen').click()
 

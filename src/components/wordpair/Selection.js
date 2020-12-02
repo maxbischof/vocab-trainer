ff5d41bcd4a23ff1601a1eba606a6ddd8e2d9592
Max <max@bischof.dk>
2020-12-02 09:11:13 +0100

    Change fontsize and bordersize of selection in relation to diameter of selectioN

--- src/components/wordpair/Selection.js
+++ src/components/wordpair/Selection.js
@@ -3,12 +3,17 @@ import styled from 'styled-components'
 
 export default function Selection({ text, active, color, diameter }) {
   const activeColor = color.substring(0, color.length - 2) + '0.44)'
+  const borderSize = (parseInt(diameter) * 1.5 - parseInt(diameter)) / 2
+  const fontSize = parseInt(diameter) / 2
+
   return (
     <Circle
       active={active}
       color={color}
       activeColor={activeColor}
       diameter={diameter}
+      borderSize={borderSize}
+      fontSize={fontSize}
     >
       {text}
     </Circle>
@@ -25,9 +30,11 @@ const Circle = styled.div`
   align-content: center;
   justify-content: center;
   font-weight: 700;
-  font-size: 36px;
+  font-size: ${(props) => props.fontSize + 'px'};
   background-clip: padding-box;
   border: ${(props) =>
-    props.active ? '16px solid ' + props.activeColor + ';' : ''};
+    props.active
+      ? props.borderSize + 'px solid ' + props.activeColor + ';'
+      : ''};
   cursor: pointer;
 `
